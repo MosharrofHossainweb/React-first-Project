cb8745621be72be15ee479d1b1d0ba8510e21c84
Mosharrof Hossain <mosharrofhossainpdl@gmail.com>
2024-10-01 20:25:49 +0600

    Your commit message

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -14,25 +14,28 @@ const Banner = () => {
                     <div className="banner_image">
                         <img src="Images\bannner_img.png" alt="banner_imgaes" />
                     </div>
-                    <div className="banner_text w-[540px] pl-[10px] ">
+                    <div className="banner_text w-[300px] md:w-[540px] pl-[10px] ">
                         <CommonHead commonHeadtext={'Normal to oily skin'}/>
                         <h1 className="text-[22px] lg:text-[72px] mt-5 font-normal font-ptSerif text-secondaryColor w-[300px] lg:w-[540px] ">Men’s Sunscreen
                         hand & body</h1>
                         <p className="font-inter text-[14px] lg:text-[18px] mt-[20px] mb-5 lg:mb-[64px] text-[#A0A79A] w-[300px] lg:w-[526px] ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'</p>
                                 <CommontButton commonButtontext={'Add to Cart'} commonButtonEffect={'hover:bg-blue-900 active:scale-[1.1]'}/>
-                                   <div className="counter_format flex gap-[30px]">
+                        <div className="counter_box ">
+
+                            <div className="counter_format flex-wrap flex gap-[15px] md:gap-[30px]">
         
-                                <div className="div flex relative">
+                                <div className="div flex ">
         
-                                <CommontCounter counterNumber={4} counterIcon={<FaStar />}counterText={'Rating'}/>  <CommontCounter counterNumber={6}/>
-                                 <h2 className="text-3xl pt-[58px] absolute right-[34px] ">,</h2>
+                                <CommontCounter counterNumber={4} countComma={','} counterIcon={<FaStar />}counterText={'Rating'}/> <CommontCounter counterNumber={6}/>
+                                 
                                     </div>
-                                <div className="counter flex gap-[49px]">
+                                <div className="counter  flex gap-[20px] md:gap-[49px]">
         
                                 <CommontCounter counterNumber={200} counterIcon={<FaShoppingBag />} countPlus={'+'} counterText={'Product solds'} />
                                 <CommontCounter counterNumber={99} counterIcon={<BsEmojiSmileFill />} countPlus={'+'} counterText={'Review'} />
                                 </div>
                            </div>
+                        </div>
                     </div>
                     </div>
             </div>
